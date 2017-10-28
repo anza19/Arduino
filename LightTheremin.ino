50b219cd9bf7f2e23e83f1af645ec4bc0979aedc
Anza <anza.k.9539@gmail.com>
2017-10-28 19:53:21 -0400

    Creating music with and LDR and a piezo

---
+++ LightTheremin.ino
@@ -0,0 +1,26 @@
+int sensorValue;
+int sensorLow = 1023;
+int sensorHigh = 0;
+const int ledPIN = 13;
+
+void setup() {
+  pinMode(ledPIN, OUTPUT);
+  digitalWrite(ledPIN, HIGH);
+  while (millis() < 5000) {
+    sensorValue = analogRead(A0);
+    if (sensorValue > sensorHigh) {
+      sensorHigh = sensorValue;
+    }
+    if (sensorValue < sensorLow) {
+      sensorLow = sensorValue;
+    }
+  }
+  digitalWrite(ledPIN, LOW);
+}
+void loop() {
+  sensorValue = analogRead(A0);
+  int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 5000);
+  tone(8, pitch, 50);
+  delay(50);
+}
+
