9f675050193302289fe574be6f6466220a28107f
Anza <anza.k.9539@gmail.com>
2017-10-28 16:05:16 -0400

    Turn LEDs on with switch
    Turns on LEDs with switch.

---
+++ TurnLEDsWithSwitch.ino
@@ -0,0 +1,52 @@
+//initializing our LEDs
+int redLED = 7;
+int yellowLED = 6;
+int blueLED = 5;
+int greenLED = 4;
+int switchState;
+int switchPos = 2;
+
+//function that assigns our LEDs to their pinMode()
+void assignLEDs(int led1, int led2, int led3, int led4);
+
+void setup() {
+  assignLEDs(redLED, yellowLED, blueLED, greenLED);
+  pinMode(switchPos, INPUT);
+}
+
+void loop() {
+  //turn an LED on for a 1 second
+  //turn off, turn the next on
+  switchState = digitalRead(switchPos);
+  if (switchState == HIGH) {
+    digitalWrite(redLED, HIGH);
+    delay(1000);
+    digitalWrite(redLED, LOW);
+    digitalWrite(yellowLED, HIGH);
+    delay(1000);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(blueLED, HIGH);
+    delay(1000);
+    digitalWrite(blueLED, LOW);
+    digitalWrite(greenLED, HIGH);
+    delay(1000);
+    digitalWrite(greenLED, LOW);
+
+  } else if(switchState == LOW) {
+    digitalWrite(redLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(blueLED, LOW);
+    digitalWrite(greenLED, LOW);
+
+  }
+}
+
+void assignLEDs(int led1, int led2, int led3, int led4) {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+}
+
+
+
