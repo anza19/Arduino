d111d67d5823a047016c01dfbdf17369b4ccfaa6
Anza <anza.k.9539@gmail.com>
2017-10-28 15:12:05 -0400

    Blue Blink
    Executing simple blink with a blue LED.

---
+++ SimpleBlink.ino
@@ -0,0 +1,19 @@
+//setting the value of led pin
+int blueLED = 6;
+void setup() {
+  // put your setup code here, to run once:
+  //pinMode assigns whether our pin is OUTPUT or INPUT
+  //LEDS == OUTPUT
+  pinMode(blueLED, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //simple ON/OFF logic
+  digitalWrite(blueLED, HIGH);
+  delay(1000);
+  digitalWrite(blueLED, LOW);
+  delay(1000);
+
+}
